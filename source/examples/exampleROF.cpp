068311549b3cdacd3bb4862a378e850ff7e6b442
Lars Haalck <l_haal02@uni-muenster.de>
2017-01-22 13:43:06 +0100

    clean up exampleROF.cpp and .cu

--- source/examples/exampleROF.cpp
+++ source/examples/exampleROF.cpp
@@ -23,70 +23,69 @@ typedef float floatingType;
 
 int main()
 {
-	float weightDataTerm = 1.0;
-	float weightRegularizer = 0.1;
+	float weightDataTerm = 1.0f;
+	float weightRegularizer = 0.1f;
 
 	flexBox<floatingType> mainObject;
 	mainObject.verbose = 1;
 
 	//read original image
-	CImg<float> imageOriginal(sampleFiles.imgA);
+	CImg<unsigned char> imageOriginal(sampleFiles.imgTest);
 
-	CImg<float>	imageOriginalGray(imageOriginal.width(), imageOriginal.height(), 1, 1, 0);
-	//convert to gray
+	CImg<unsigned char>	imageOriginalGray(imageOriginal.width(), imageOriginal.height(), 1, 1, 0);
+	//convert to gray using luminosity method 
 	for (int i = 0; i < imageOriginal.height(); ++i)
 	{
 		for (int j = 0; j < imageOriginal.width(); ++j)
 		{
-			int R = (int)imageOriginal(i, j, 0, 0);
-			int G = (int)imageOriginal(i, j, 0, 1);
-			int B = (int)imageOriginal(i, j, 0, 2);
+			unsigned char R = imageOriginal(i, j, 0, 0);
+			unsigned char G = imageOriginal(i, j, 0, 1);
+			unsigned char B = imageOriginal(i, j, 0, 2);
 
-			int grayValue = (int)(0.299*R + 0.587*G + 0.114*B);
+			unsigned char grayValue = static_cast<unsigned char>(0.299*R + 0.587*G + 0.114*B);
 
-			imageOriginalGray[j*imageOriginal.width() + i] = grayValue;
+			imageOriginalGray(i,j) = grayValue;
 		}
 	}
 
 
-	CImg<float> imageNoise = imageOriginalGray;
-	imageNoise.noise(30);
+	CImg<unsigned char> imageNoise = imageOriginalGray;
+	imageNoise.noise(20);
 
 	//add primal variable
-	std::vector<int> _dims;
-	_dims.push_back(imageNoise.height()); _dims.push_back(imageNoise.width());
-	mainObject.addPrimalVar(_dims);
+	std::vector<int> dims;
+	dims.push_back(imageNoise.height());
+	dims.push_back(imageNoise.width());
+	mainObject.addPrimalVar(dims);
 
 	//number of elements in vector
 	int nPx = imageNoise.height() * imageNoise.width();
 
 	//add empty term for primal var, because we want to solve the fully dualized problem
-	std::vector<int> _correspondingPrimals;
-	_correspondingPrimals.push_back(0);
+	std::vector<int> correspondingPrimals;
+	correspondingPrimals.push_back(0);
 
-	mainObject.addPrimal(new flexTermPrimal<floatingType>(1, 1, primalEmptyProx), _correspondingPrimals);
+	mainObject.addPrimal(new flexTermPrimal<floatingType>(1, 1, primalEmptyProx), correspondingPrimals);
 
 	//add dualized data term:
 	std::vector<flexLinearOperator<floatingType>*> operatorList;
 	operatorList.push_back(new flexIdentityOperator<floatingType>(nPx, nPx, false));
 
-	//convert input image to grey value and add to vector data
+	//reshape image to vector and normalize to [0, 1]
 	std::vector<floatingType> f(nPx, 0.0f);
 	for (int i = 0; i < imageNoise.height(); ++i)
 	{
 		for (int j = 0; j < imageNoise.width(); ++j)
 		{
-			f[i*imageNoise.width() + j] = ((floatingType)imageOriginalGray[i*imageOriginal.width() + j]) / 255.0f;
+			f[i*imageNoise.width() + j] = static_cast<floatingType>(imageNoise[i*imageOriginal.width() + j]) / static_cast<floatingType>(255);
 		}
 	}
 
 	std::vector<std::vector<floatingType>> fList = std::vector<std::vector<floatingType>>();
 	fList.push_back(f);
 
-	//CImgDisplay main_disp(image, "Original"), draw_dispGr(gray, "Grey");
-
 	flexProx<floatingType>* myProx = new flexProxDualDataL2<floatingType>();
-	mainObject.addDual(new flexTermDual<floatingType>(myProx, weightDataTerm, (int) _correspondingPrimals.size(), operatorList, fList), _correspondingPrimals);
+	mainObject.addDual(new flexTermDual<floatingType>(myProx, weightDataTerm, static_cast<int>(correspondingPrimals.size()), operatorList, fList), correspondingPrimals);
 
 	//add dualized regularizer
 	operatorList.clear();
@@ -95,7 +94,7 @@ int main()
 	operatorList.push_back(new flexGradientOperator<floatingType>(mainObject.getDims(0), 1, 0));
 
 	flexProx<floatingType>* myProx2 = new flexProxDualL1Iso<floatingType>();
-	mainObject.addDual(new flexTermDual<floatingType>(myProx2, weightRegularizer, 1, operatorList), _correspondingPrimals);
+	mainObject.addDual(new flexTermDual<floatingType>(myProx2, weightRegularizer, 1, operatorList), correspondingPrimals);
 
 	mainObject.runAlgorithm();
 
@@ -106,14 +105,13 @@ int main()
 	{
 		for (int j = 0; j < imageNoise.width(); ++j)
 		{
-			imageResult(i, j, 0, 0) = (int)(flexResult[j*imageNoise.width() + i] * 255);
+			imageResult(i, j, 0, 0) = static_cast<unsigned char>(flexResult[j*imageNoise.width() + i] * 255);
 		}
 	}
 
 	CImgDisplay main_disp(imageOriginalGray, "Original"), draw_dispGr(imageNoise, "Noise"), draw_dispR(imageResult, "Result");
-	while (!main_disp.is_closed() && !draw_dispGr.is_closed() &&
-		!main_disp.is_keyESC() && !draw_dispGr.is_keyESC() && !main_disp.is_keyQ() && !draw_dispGr.is_keyQ()) {
-
+	while (!main_disp.is_closed() && !draw_dispGr.is_closed() && !main_disp.is_keyESC() && !draw_dispGr.is_keyESC() && !main_disp.is_keyQ() && !draw_dispGr.is_keyQ())
+	{
 		// Handle display window resizing (if any)
 		if (main_disp.is_resized()) main_disp.resize().display(imageOriginalGray);
 		if (draw_dispGr.is_resized()) draw_dispGr.resize().display(imageNoise);
