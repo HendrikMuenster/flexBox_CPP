f734327d911a05409ec5bd92490d4a7cf495439f
hendrikMuenster <hendrik.dirks@wwu.de>
2017-02-13 13:43:32 +0100

    - CUDA support for labeling - 3D support for C++ gradient operator

--- source/operator/flexGradientOperator.h
+++ source/operator/flexGradientOperator.h
@@ -46,8 +46,344 @@ public:
 
 	}
 
+	//
+	//
+	//  3d cases
+	//
+	//
 
+	void dxp3d(const Tdata &input, Tdata &output, mySign s)
+	{
+		int sizeZ = this->inputDimension[2];
+		int sizeY = this->inputDimension[1];
+		int sizeX = this->inputDimension[0] - 1;
 
+		#pragma omp parallel for
+		for (int k = 0; k < sizeZ; ++k)
+		{
+			for (int j = 0; j < sizeY; ++j)
+			{
+				for (int i = 0; i < sizeX; ++i)
+				{
+					const int tmpIndex = this->index3DtoLinear(i, j, k);
+
+					switch (s)
+					{
+					case PLUS:
+					{
+						output[tmpIndex] += input[tmpIndex + 1] - input[tmpIndex];
+						break;
+					}
+					case MINUS:
+					{
+						output[tmpIndex] -= input[tmpIndex + 1] - input[tmpIndex];
+						break;
+					}
+					case EQUALS:
+					{
+						output[tmpIndex] = input[tmpIndex + 1] - input[tmpIndex];
+						break;
+					}
+					}
+				}
+			}
+		}
+	}
+
+	void dyp3d(const Tdata &input, Tdata &output, mySign s)
+	{
+		int sizeZ = this->inputDimension[2]; 
+		int sizeY = this->inputDimension[1] - 1;
+		int sizeX = this->inputDimension[0];
+
+		#pragma omp parallel for
+		for (int k = 0; k < sizeZ; ++k)
+		{
+			for (int j = 0; j < sizeY; ++j)
+			{
+				for (int i = 0; i < sizeX; ++i)
+				{
+					const int tmpIndex1 = this->index3DtoLinear(i, j, k);
+					const int tmpIndex2 = this->index3DtoLinear(i, j + 1, k);
+
+					switch (s)
+					{
+					case PLUS:
+					{
+						output[tmpIndex1] += input[tmpIndex2] - input[tmpIndex1];
+						break;
+					}
+					case MINUS:
+					{
+						output[tmpIndex1] -= input[tmpIndex2] - input[tmpIndex1];
+						break;
+					}
+					case EQUALS:
+					{
+						output[tmpIndex1] = input[tmpIndex2] - input[tmpIndex1];
+						break;
+					}
+					}
+				}
+			}
+		}
+	}
+
+	void dzp3d(const Tdata &input, Tdata &output, mySign s)
+	{
+		int sizeZ = this->inputDimension[2] - 1;
+		int sizeY = this->inputDimension[1];
+		int sizeX = this->inputDimension[0];
+
+		#pragma omp parallel for
+		for (int k = 0; k < sizeZ; ++k)
+		{
+			for (int j = 0; j < sizeY; ++j)
+			{
+				for (int i = 0; i < sizeX; ++i)
+				{
+					const int tmpIndex1 = this->index3DtoLinear(i, j, k);
+					const int tmpIndex2 = this->index3DtoLinear(i, j, k + 1);
+
+					switch (s)
+					{
+					case PLUS:
+					{
+						output[tmpIndex1] += input[tmpIndex2] - input[tmpIndex1];
+						break;
+					}
+					case MINUS:
+					{
+						output[tmpIndex1] -= input[tmpIndex2] - input[tmpIndex1];
+						break;
+					}
+					case EQUALS:
+					{
+						output[tmpIndex1] = input[tmpIndex2] - input[tmpIndex1];
+						break;
+					}
+					}
+				}
+			}
+		}
+	}
+
+	void dxp3dTransposed(const Tdata &input, Tdata &output, mySign s)
+	{
+		const int sizeZ = this->inputDimension[2];
+		const int sizeY = this->inputDimension[1];
+		const int sizeX = this->inputDimension[0] - 1;
+
+		#pragma omp parallel for
+		for (int k = 0; k < sizeZ; ++k)
+		{
+			for (int j = 0; j < sizeY; ++j)
+			{
+				for (int i = 1; i < sizeX; ++i)
+				{
+					int tmpIndex = this->index3DtoLinear(i, j, k);
+
+					switch (s)
+					{
+					case PLUS:
+					{
+						output[tmpIndex] += -(input[tmpIndex] - input[tmpIndex - 1]);
+						break;
+					}
+					case MINUS:
+					{
+						output[tmpIndex] -= -(input[tmpIndex] - input[tmpIndex - 1]);
+						break;
+					}
+					case EQUALS:
+					{
+						output[tmpIndex] = -(input[tmpIndex] - input[tmpIndex - 1]);
+						break;
+					}
+					}
+				}
+			}
+		}
+
+		#pragma omp parallel for
+		for (int k = 0; k < this->inputDimension[2]; ++k)
+		{
+			for (int j = 0; j < this->inputDimension[1]; ++j)
+			{
+				const int index1 = this->index3DtoLinear(0, j, k);
+				const int index2 = this->index3DtoLinear(this->inputDimension[0] - 1, j, k);
+				const int index3 = this->index3DtoLinear(this->inputDimension[0] - 2, j, k);
+
+				switch (s)
+				{
+				case PLUS:
+				{
+					output[index1] += -input[index1];
+					output[index2] += input[index3];
+					break;
+				}
+				case MINUS:
+				{
+					output[index1] -= -input[index1];
+					output[index2] -= input[index3];
+					break;
+				}
+				case EQUALS:
+				{
+					output[index1] = -input[index1];
+					output[index2] = input[index3];
+					break;
+				}
+				}
+			}
+		}
+	}
+
+	void dyp3dTransposed(const Tdata &input, Tdata &output, mySign s)
+	{
+		const int sizeZ = this->inputDimension[2];
+		const int sizeY = this->inputDimension[1] - 1;
+		const int sizeX = this->inputDimension[0];
+
+		#pragma omp parallel for
+		for (int k = 0; k < sizeZ; ++k)
+		{
+			for (int j = 1; j < sizeY; ++j)
+			{
+				for (int i = 0; i < sizeX; ++i)
+				{
+					const int tmpIndex1 = this->index3DtoLinear(i, j, k);
+					const int tmpIndex2 = this->index3DtoLinear(i, j - 1, k);
+
+					switch (s)
+					{
+					case PLUS:
+					{
+						output[tmpIndex1] += -(input[tmpIndex1] - input[tmpIndex2]);
+						break;
+					}
+					case MINUS:
+					{
+						output[tmpIndex1] -= -(input[tmpIndex1] - input[tmpIndex2]);
+						break;
+					}
+					case EQUALS:
+					{
+						output[tmpIndex1] = -(input[tmpIndex1] - input[tmpIndex2]);
+						break;
+					}
+					}
+				}
+			}
+		}
+
+		#pragma omp parallel for
+		for (int k = 0; k < this->inputDimension[2]; ++k)
+		{
+			for (int i = 0; i < this->inputDimension[0]; ++i)
+			{
+				const int index1 = this->index3DtoLinear(i, 0, k);
+				const int index2 = this->index3DtoLinear(i, this->inputDimension[1] - 1, k);
+				const int index3 = this->index3DtoLinear(i, this->inputDimension[1] - 2, k);
+
+				switch (s)
+				{
+				case PLUS:
+				{
+					output[index1] += -input[index1];
+					output[index2] += input[index3];
+					break;
+				}
+				case MINUS:
+				{
+					output[index1] -= -input[index1];
+					output[index2] -= input[index3];
+					break;
+				}
+				case EQUALS:
+				{
+					output[index1] = -input[index1];
+					output[index2] = input[index3];
+					break;
+				}
+				}
+			}
+		}
+	}
+
+	void dzp3dTransposed(const Tdata &input, Tdata &output, mySign s)
+	{
+		const int sizeZ = this->inputDimension[2] - 1;
+		const int sizeY = this->inputDimension[1];
+		const int sizeX = this->inputDimension[0];
+
+		#pragma omp parallel for
+		for (int k = 1; k < sizeZ; ++k)
+		{
+			for (int j = 0; j < sizeY; ++j)
+			{
+				for (int i = 0; i < sizeX; ++i)
+				{
+					const int tmpIndex1 = this->index3DtoLinear(i, j, k);
+					const int tmpIndex2 = this->index3DtoLinear(i, j, k - 1);
+
+					switch (s)
+					{
+					case PLUS:
+					{
+						output[tmpIndex1] += -(input[tmpIndex1] - input[tmpIndex2]);
+						break;
+					}
+					case MINUS:
+					{
+						output[tmpIndex1] -= -(input[tmpIndex1] - input[tmpIndex2]);
+						break;
+					}
+					case EQUALS:
+					{
+						output[tmpIndex1] = -(input[tmpIndex1] - input[tmpIndex2]);
+						break;
+					}
+					}
+				}
+			}
+		}
+
+		#pragma omp parallel for
+		for (int j = 0; j < this->inputDimension[1]; ++j)
+		{
+			for (int i = 0; i < this->inputDimension[0]; ++i)
+			{
+				const int index1 = this->index3DtoLinear(i, j, 0);
+				const int index2 = this->index3DtoLinear(i, j, this->inputDimension[2] - 1);
+				const int index3 = this->index3DtoLinear(i, j, this->inputDimension[2] - 2);
+
+				switch (s)
+				{
+				case PLUS:
+				{
+					output[index1] += -input[index1];
+					output[index2] += input[index3];
+					break;
+				}
+				case MINUS:
+				{
+					output[index1] -= -input[index1];
+					output[index2] -= input[index3];
+					break;
+				}
+				case EQUALS:
+				{
+					output[index1] = -input[index1];
+					output[index2] = input[index3];
+					break;
+				}
+				}
+			}
+		}
+	}
+
+	//2d cases
 	void dxp2d(const Tdata &input, Tdata &output, mySign s)
 	{
 		int sizeY = this->inputDimension[1];
@@ -316,37 +652,86 @@ public:
 				}
 			}
 		}
+		else if (this->inputDimension.size() == 3)
+		{
+			if (this->gradDirection == 0)
+			{
+				if (transposed == false)
+				{
+#ifdef __CUDACC__
+//					dxp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+#else
+					this->dxp3d(input, output, s);
+#endif
+				}
+				else
+				{
+#ifdef __CUDACC__
+//					dxp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+#else
+					this->dxp3dTransposed(input, output, s);
+#endif
+				}
+			}
+			else if (this->gradDirection == 1)
+			{
+				if (transposed == false)
+				{
+#ifdef __CUDACC__
+//					dyp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+#else
+					this->dyp3d(input, output, s);
+#endif
+				}
+				else
+				{
+#ifdef __CUDACC__
+//					dyp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+#else
+					this->dyp3dTransposed(input, output, s);
+#endif
+				}
+			}
+			else if (this->gradDirection == 2)
+			{
+				if (transposed == false)
+				{
+#ifdef __CUDACC__
+					//					dyp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+#else
+					this->dzp3d(input, output, s);
+#endif
+				}
+				else
+				{
+#ifdef __CUDACC__
+					//					dyp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+#else
+					this->dzp3dTransposed(input, output, s);
+#endif
+				}
+			}
+		}
 		else
 		{
-            printf("Gradient not implemented for dim!=2");
+			printf("Gradient not implemented for dim!={2,3}\n");
 			//todo
 		}
 	}
 
-	void timesMinus(bool transposed, const Tdata &input, Tdata &output)
+	void doTimesMinusCPU(bool transposed, const Tdata &input, Tdata &output)
 	{
-		#ifdef __CUDACC__
-			dim3 block2d = dim3(32, 16, 1);
-			dim3 grid2d = dim3((this->inputDimension[0] + block2d.x - 1) / block2d.x, (this->inputDimension[1] + block2d.y - 1) / block2d.y, 1);
+		mySign s;
 
-			T* ptrOutput = thrust::raw_pointer_cast(output.data());
-			const T* ptrInput = thrust::raw_pointer_cast(input.data());
-		#endif
-		
-        mySign s;
-        int s2;
-		
 		if (this->isMinus)
-        {
-            s = MINUS;
-            s2 = SIGN_MINUS;
+		{
+			s = MINUS;
 		}
-        else
-        {
-            s = PLUS;
-            s2 = SIGN_PLUS;
-        }
-		
+		else
+		{
+			s = PLUS;
+		}
+
 		// flip sign and transposed
 		if (this->type == backward)
 		{
@@ -354,11 +739,112 @@ public:
 			if (s == MINUS)
 			{
 				s = PLUS;
-				s2 = SIGN_PLUS;
 			}
 			else
 			{
 				s = MINUS;
+			}
+		}
+
+		if (this->inputDimension.size() == 2)
+		{
+			if (this->gradDirection == 0)
+			{
+				if (transposed == false)
+				{
+					this->dxp2d(input, output, s);
+				}
+				else
+				{
+					this->dxp2dTransposed(input, output, s);
+				}
+			}
+			else if (this->gradDirection == 1)
+			{
+				if (transposed == false)
+				{
+					this->dyp2d(input, output, s);
+				}
+				else
+				{
+					this->dyp2dTransposed(input, output, s);
+				}
+			}
+		}
+		else if (this->inputDimension.size() == 3)
+		{
+			if (this->gradDirection == 0)
+			{
+				if (transposed == false)
+				{
+					this->dxp3d(input, output, s);
+				}
+				else
+				{
+					this->dxp3dTransposed(input, output, s);
+				}
+			}
+			else if (this->gradDirection == 1)
+			{
+				if (transposed == false)
+				{
+					this->dyp3d(input, output, s);
+				}
+				else
+				{
+					this->dyp3dTransposed(input, output, s);
+				}
+			}
+			else if (this->gradDirection == 2)
+			{
+				if (transposed == false)
+				{
+					this->dzp3d(input, output, s);
+				}
+				else
+				{
+					this->dzp3dTransposed(input, output, s);
+				}
+			}
+		}
+		else
+		{
+			printf("Gradient not implemented for dim!={2,3}\n");
+			//todo
+		}
+	}
+
+	#ifdef __CUDACC__
+	void doTimesMinusCUDA(bool transposed, const Tdata &input, Tdata &output)
+	{
+
+		dim3 block2d = dim3(32, 16, 1);
+		dim3 grid2d = dim3((this->inputDimension[0] + block2d.x - 1) / block2d.x, (this->inputDimension[1] + block2d.y - 1) / block2d.y, 1);
+
+		T* ptrOutput = thrust::raw_pointer_cast(output.data());
+		const T* ptrInput = thrust::raw_pointer_cast(input.data());
+
+		int s2;
+
+		if (this->isMinus)
+		{
+			s2 = SIGN_MINUS;
+		}
+		else
+		{
+			s2 = SIGN_PLUS;
+		}
+
+		// flip sign and transposed
+		if (this->type == backward)
+		{
+			transposed = !transposed;
+			if (s == MINUS)
+			{
+				s2 = SIGN_PLUS;
+			}
+			else
+			{
 				s2 = SIGN_MINUS;
 			}
 		}
@@ -369,46 +855,76 @@ public:
 			{
 				if (transposed == false)
 				{
-					#ifdef __CUDACC__
-						dxp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
-					#else
-						this->dxp2d(input, output, s);
-					#endif
+					dxp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
 				}
 				else
 				{
-					#ifdef __CUDACC__
-						dxp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
-					#else
-						this->dxp2dTransposed(input, output, s);
-					#endif
+					dxp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
 				}
 			}
 			else if (this->gradDirection == 1)
 			{
 				if (transposed == false)
 				{
-					#ifdef __CUDACC__
-						dyp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
-					#else
-						this->dyp2d(input, output, s);
-					#endif
+					dyp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
 				}
 				else
 				{
-					#ifdef __CUDACC__
-						dyp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
-					#else
-						this->dyp2dTransposed(input, output, s);
-					#endif
+					dyp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+				}
+			}
+		}
+		else if (this->inputDimension.size() == 3)
+		{
+			if (this->gradDirection == 0)
+			{
+				if (transposed == false)
+				{
+					//					dxp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+				}
+				else
+				{
+					//					dxp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+				}
+			}
+			else if (this->gradDirection == 1)
+			{
+				if (transposed == false)
+				{
+					//					dyp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+				}
+				else
+				{
+					//					dyp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+				}
+			}
+			else if (this->gradDirection == 2)
+			{
+				if (transposed == false)
+				{
+					//					dyp2dCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
+				}
+				else
+				{
+					//					dyp2dTransposedCUDA << <grid2d, block2d >> >(ptrOutput, ptrInput, this->inputDimension[0], this->inputDimension[1], s2);
 				}
 			}
 		}
 		else
-        {
-            printf("Gradient not implemented for dim!=2");
+		{
+			printf("Gradient not implemented for dim!={2,3}\n");
 			//todo
 		}
+	}
+	#endif
+
+	void timesMinus(bool transposed, const Tdata &input, Tdata &output)
+	{
+		#ifdef __CUDACC__
+		this->doTimesMinusCUDA(transposed,input, output);
+		#else
+		this->doTimesMinusCPU(transposed, input, output);
+		#endif
 	}
 
 	T getMaxRowSumAbs(bool transposed)
@@ -422,6 +938,11 @@ public:
 		std::vector<T> result(this->getNumRows(),(T)2);
 
 		return result;
+	}
+
+	int index3DtoLinear(int i, int j, int k)
+	{
+		return (i + j*this->inputDimension[0] + k*this->inputDimension[0] * this->inputDimension[1]);
 	}
 
 	int index2DtoLinear(int i, int j)
--- source/prox/flexProxDualLabeling.h
+++ source/prox/flexProxDualLabeling.h
@@ -3,6 +3,84 @@
 
 #include "flexProx.h"
 
+//put cuda kernel
+#ifdef __CUDACC__
+#define MAX_NUMBER_LABELS 16
+
+template<typename T>
+__global__ void dualLabelingProxCUDA(T** listYPtr, T** listYTildePtr, T** listFPtr, T** listSigmaPtr, int* dualNumbers, int numElements, int numDualVars)
+{
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if (i >= numElements)
+		return;
+
+
+	T tmpVector[MAX_NUMBER_LABELS]; 
+	T tmpVector2[MAX_NUMBER_LABELS]; 
+
+	//copy from yTilde
+	for (int j = 0; j < numDualVars; j++)
+	{
+		tmpVector[j] = (listYTildePtr[dualNumbers[j]][i] - listFPtr[j][i]) / listSigmaPtr[dualNumbers[j]][i];
+		tmpVector2[j] = tmpVector[j];
+	}
+
+	//sort tmpVector
+	for (int c = 0 ; c < ( numDualVars - 1 ) ; c++ )
+	{
+		int position = c;
+
+		for (int d = c + 1 ; d < numDualVars ; d++ )
+		{
+			if ( tmpVector[position] > tmpVector[d] )
+			{
+				position = d;
+			}
+		}
+		if (position != c)
+		{
+			T swap = tmpVector[c];
+			tmpVector[c] = tmpVector[position];
+			tmpVector[position] = swap;
+		}
+   }
+
+	T sumY = (T)0;
+	T tOpt;
+
+	int j = numDualVars - 2;
+	while (j >= 0)
+	{
+		sumY = sumY + tmpVector[j + 1];
+
+		tOpt = (sumY - 1) / (numDualVars - (j + 1));
+
+		if (tOpt >= tmpVector[j])
+		{
+			break;
+		}
+		else
+		{
+			j = j - 1;
+		}
+	}
+
+	if (j < 0)
+	{
+		tOpt = (sumY + tmpVector[0] - 1) / numDualVars;
+	}
+
+	//write result
+	for (int j = 0; j < numDualVars; j++)
+	{
+		listYPtr[dualNumbers[j]][i] = listYTildePtr[dualNumbers[j]][i] - listSigmaPtr[dualNumbers[j]][i] * max(tmpVector2[j] - tOpt, (T)0);
+	}
+}
+
+#endif
+
+
 template<typename T>
 class flexProxDualLabeling : public flexProx<T>
 {
@@ -22,9 +100,7 @@ public:
 		if (VERBOSE > 0) printf("Destructor prox\n!");
 	}
 	
-	//put cuda functor here
-    #ifdef __CUDACC__
-    #endif
+
 	
 	
 
@@ -34,8 +110,33 @@ public:
 
 	void applyProx(T alpha, flexBoxData<T>* data, const std::vector<int> &dualNumbers, const std::vector<int> &primalNumbers, std::vector<Tdata> &fList)
 	{
-#ifdef __CUDACC__
-			printf("flexProxDualLabeling not implemented for CUDA\n");
+		#ifdef __CUDACC__
+			int numElements = (int)data->yTilde[dualNumbers[0]].size();
+			int numDualVars = (int)dualNumbers.size();
+
+			thrust::device_vector<T*> listYPtr(numDualVars);
+			thrust::device_vector<T*> listYTildePtr(numDualVars);
+			thrust::device_vector<T*> listFPtr(numDualVars);
+			thrust::device_vector<T*> listSigmaPtr(numDualVars);
+
+			thrust::device_vector<int> dualNumbersCUDA(dualNumbers);
+				
+			for (int j = 0; j < numDualVars; j++)
+			{
+				listYPtr[j] = thrust::raw_pointer_cast(data->y[dualNumbers[j]].data());
+				listYTildePtr[j] = thrust::raw_pointer_cast(data->yTilde[dualNumbers[j]].data());
+				listFPtr[j] = thrust::raw_pointer_cast(fList[j].data());
+				listSigmaPtr[j] = thrust::raw_pointer_cast(data->sigmaElt[dualNumbers[j]].data());
+			}
+
+			T** YPtr = thrust::raw_pointer_cast(listYPtr.data());
+			T** YTildePtr = thrust::raw_pointer_cast(listYTildePtr.data());
+			T** FPtr = thrust::raw_pointer_cast(listFPtr.data());
+			T** SigmaPtr = thrust::raw_pointer_cast(listSigmaPtr.data());
+
+			int* dualNumbersCUDAPtr = thrust::raw_pointer_cast(dualNumbersCUDA.data());
+
+			dualLabelingProxCUDA << <(int)ceil(numElements / 512), 512 >> >(YPtr,YTildePtr,FPtr,SigmaPtr,dualNumbersCUDAPtr,numElements,numDualVars);
 		#else
 			int numElements = (int)data->yTilde[dualNumbers[0]].size();
 			
@@ -94,8 +195,6 @@ public:
 					}
 				}
 			}
-			
-			
 		#endif
 	}
 };
