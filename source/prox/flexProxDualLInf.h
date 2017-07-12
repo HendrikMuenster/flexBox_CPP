dade27377b24906ef16f11c5968ee8d320bcbe04
Lars Haalck <lars.haalck@gmail.com>
2017-07-12 17:52:14 +0200

    first thrust impl for LInf prox

--- source/prox/flexProxDualLInf.h
+++ source/prox/flexProxDualLInf.h
@@ -31,87 +31,86 @@ public:
     }
 
 #ifdef __CUDACC__
-    /*struct flexProxDualL1IsoDim1Functor
+    struct flexProxDualLInfFunctor
     {
         __host__ __device__
-            flexProxDualL1IsoDim1Functor(T alpha) : alpha(alpha) {}
+            flexProxDualLInfFunctor(T aTheta) : theta(aTheta) {}
 
         template <typename Tuple>
         __host__ __device__
             void operator()(Tuple t)
         {
-            thrust::get<0>(t) = thrust::get<1>(t) / max((T)1, fabs(thrust::get<1>(t)) / this->alpha);
+            thrust::get<0>(t) = (thrust::get<0>(t) > 0 ? 1 : -1) * max((T)0, abs(thrust::get<1>(t) - theta));
+        }
+
+        T theta;
+    };
+
+    struct flexProxDualLInfFindFunctor
+    {
+        __host__ __device__
+            flexProxDualLInfFunctor(T alpha) : alpha(alpha) {}
+
+        template <typename Tuple>
+        __host__ __device__
+            void operator()(Tuple t)
+        {
+            return thrust::get<0>(t) > (thrust::get<1>(t) - alpha) / thrust::get<2>(t)
         }
 
         T alpha;
     };
 
-    struct flexProxDualL1IsoDim2Functor
+    template<typename T>
+    struct absFunctor : public unary_function<T, T>
     {
-        __host__ __device__
-            flexProxDualL1IsoDim2Functor(T alpha) : alpha(alpha) {}
-
         template <typename Tuple>
         __host__ __device__
-            void operator()(Tuple t)
+            T operator()(const T &)
         {
-            T norm = max((T)1, sqrt(pow(thrust::get<2>(t), (int)2) + pow(thrust::get<3>(t), (int)2)) / this->alpha);
-
-            thrust::get<0>(t) = thrust::get<2>(t) / norm;
-            thrust::get<1>(t) = thrust::get<3>(t) / norm;
+            return x < T(0) ? -x : x;
         }
-
-        T alpha;
     };
-
-    struct flexProxDualL1IsoDim3Functor
-    {
-        __host__ __device__
-            flexProxDualL1IsoDim3Functor(T alpha) : alpha(alpha) {}
-
-        template <typename Tuple>
-        __host__ __device__
-            void operator()(Tuple t)
-        {
-            T norm = max((T)1, sqrt(pow(thrust::get<3>(t), (int)2) + pow(thrust::get<4>(t), (int)2) + pow(thrust::get<5>(t), (int)2)) / this->alpha);
-
-            thrust::get<0>(t) = thrust::get<3>(t) / norm;
-            thrust::get<1>(t) = thrust::get<4>(t) / norm;
-            thrust::get<2>(t) = thrust::get<5>(t) / norm;
-        }
-
-        T alpha;
-    };*/
 #endif
 
     void applyProx(T alpha, flexBoxData<T>* data, const std::vector<int> &dualNumbers, const std::vector<int> &primalNumbers)
     {
 #ifdef __CUDACC__
-        /*if (dualNumbers.size() == 1)
+        if (dualNumbers.size() != 1)
         {
-            auto startIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].begin(), data->yTilde[dualNumbers[0]].begin()));
-            auto endIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].end(), data->yTilde[dualNumbers[0]].end()));
-
-            thrust::for_each(startIterator, endIterator, flexProxDualL1IsoDim1Functor(alpha));
-        }
-        else if (dualNumbers.size() == 2)
-        {
-            auto startIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].begin(), data->y[dualNumbers[1]].begin(), data->yTilde[dualNumbers[0]].begin(), data->yTilde[dualNumbers[1]].begin()));
-            auto endIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].end(), data->y[dualNumbers[1]].end(), data->yTilde[dualNumbers[0]].end(), data->yTilde[dualNumbers[1]].end()));
-
-            thrust::for_each(startIterator, endIterator, flexProxDualL1IsoDim2Functor(alpha));
-        }
-        else if (dualNumbers.size() == 3)
-        {
-            auto startIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].begin(), data->y[dualNumbers[1]].begin(), data->y[dualNumbers[2]].begin(), data->yTilde[dualNumbers[0]].begin(), data->yTilde[dualNumbers[1]].begin(), data->yTilde[dualNumbers[2]].begin()));
-            auto endIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].end(), data->y[dualNumbers[1]].end(), data->y[dualNumbers[2]].end(), data->yTilde[dualNumbers[0]].end(), data->yTilde[dualNumbers[1]].end(), data->yTilde[dualNumbers[2]].end()));
-
-            thrust::for_each(startIterator, endIterator, flexProxDualL1IsoDim3Functor(alpha));
+            printf("Alert! LInf prox only defined for dim = 1");
         }
         else
         {
-            printf("Alert! Iso prox not implemented in CUDA for dim>3");
-        }*/
+            T norm = thrust::transform_reduce(data->yTilde[dualNumbers[0]].begin(), data->yTilde[dualNumbers[0]].end(), absFunctor<T>(), 0, thrust::plus<T>());
+
+            if (norm < alpha)
+            {
+                data->y[dualNumbers[0]] = data->yTilde[dualNumbers[0]];
+                return;
+            }
+
+            Tdata yTildeSort(data->yTilde[dualNumbers[0]]);
+            thrust::sort(yTildeSort.begin(), yTildeSort.end());
+            Tdata cumSum(yTildeSort);
+            thrust::inclusive_scan(yTildeSort.begin(), yTildeSort.end(), cumSum.begin());
+
+            thrust::device_vector<T> seq(data->y[dualNumbers[0]].size());
+            thrust::sequence(seq.begin(), seq.end(), 1);
+
+            auto startIterator = thrust::make_zip_iterator(thrust::make_tuple(yTildeSort.rbegin(), cumSum.rbegin(), seq.rbegin()));
+            auto endIterator = thrust::make_zip_iterator(thrust::make_tuple(yTildeSort.rend(), cumSum.rend(), seq.rend()));
+
+            auto findIt = thrust::find_if(startIterator, endIterator, flexProxDualLInfFindFunctor(alpha));
+            T theta = (thrust::get<1>(*findIt) - alpha) / thrust::get<3>(*findIt);
+            theta = std::max(static_cast<T>(0), theta);
+
+            startIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].begin(), data->yTilde[dualNumbers[0]].begin()));
+            endIterator = thrust::make_zip_iterator(thrust::make_tuple(data->y[dualNumbers[0]].end(), data->yTilde[dualNumbers[0]].end()));
+
+            thrust::for_each(startIterator, endIterator, flexProxDualLInfFunctor(alpha));
+        }
+        
 #else
         if(dualNumbers.size() != 1)
             printf("Alert! LInf prox only defined for dim = 1");
@@ -123,11 +122,9 @@ public:
 
             
             T norm = 0;
-            #pragma omp parallel for
+            #pragma omp parallel for reduction (+:norm)
             for (int i = 0; i < static_cast<int>(ySize); i++)
-            {
                 norm += fabs(ptrYtilde[i]);
-            }
 
             if (norm < alpha)
             {
@@ -136,31 +133,23 @@ public:
             }
 
             
-            auto yTildeSort = data->yTilde[dualNumbers[0]];
-            std::sort(std::begin(yTildeSort), std::end(yTildeSort), [](T lhs, T rhs) { return fabs(lhs) > fabs(rhs); });
+            auto yTildeSort = data->yTilde[dualNumbers[0]]; //copy because it needs to be sorted
+            std::sort(std::begin(yTildeSort), std::end(yTildeSort), [](T lhs, T rhs) { return fabs(lhs) > fabs(rhs); }); //sort abs descending
             Tdata cumSum = Tdata(yTildeSort.size());
+            std::partial_sum(std::begin(yTildeSort), std::end(yTildeSort), std::begin(cumSum)); //no omp because it has data dependencies
 
             T sum = 0;
             auto yTildeSortPtr = yTildeSort.data();
 
-            #pragma omp parallel for
-            for (int i = 0; i < static_cast<int>(ySize); i++)
-            {
-                sum += yTildeSortPtr[i];
-                cumSum[i] = sum - alpha;
-            }
-
-            //find maximal rho s.t. yTildeSort(rho) > cumSum(rho)/rho
-            //size_t rho = 0;
+            //find maximal index s.t. yTildeSort(rho) > (cumSum(rho) - alpha)/rho
             T theta = 0;
             auto cumSumPtr = cumSum.data();
             #pragma omp parallel for
             for (int i = static_cast<int>(ySize) - 1; i >= 0; i--)
             {
-                if (yTildeSortPtr[i] > cumSumPtr[i] / (i + 1))
+                if (yTildeSortPtr[i] > (cumSumPtr[i] - alpha)/ (i + 1))
                 {
-                    //rho = i;
-                    theta = cumSumPtr[i] / (i + 1);
+                    theta = (cumSumPtr[i] - alpha) / (i + 1);
                     break;
                 }
             }
